28f42dcaceba52f12b7258f57d6da60ef987df04
aui_tuthousend <mraihan286@gmail.com>
2024-02-09 08:09:05 +0700

    fix: home

--- japaskrip/app.js
+++ japaskrip/app.js
@@ -1,30 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const deskripsiList = [
         "merupakan sebuah lembaga yang berjalan di JL. Pagesangan",
-        "into your ocean eyes",
-        "what can i say is complicated",
-        // Tambahkan deskripsi lainnya sesuai kebutuhan
+        "depan muhamadiyah",
+        "belok kanan",
     ];
 
     let currentDeskripsiIndex = 0;
     const descriptionElement = document.getElementById("dynamicDescription");
 
     function updateDeskripsi() {
-        descriptionElement.classList.add("hide"); // Menambah kelas 'hide' untuk efek transisi
+        descriptionElement.classList.add("hide");
 
         setTimeout(function() {
             descriptionElement.textContent = deskripsiList[currentDeskripsiIndex];
             currentDeskripsiIndex = (currentDeskripsiIndex + 1) % deskripsiList.length;
 
-            descriptionElement.classList.remove("hide"); // Menghapus kelas 'hide' untuk efek transisi
-        }, 500); // Menyesuaikan waktu animasi dengan waktu transisi CSS
+            descriptionElement.classList.remove("hide");
+        }, 500);
     }
 
-    // Pertama kali, langsung menampilkan deskripsi tanpa animasi
     descriptionElement.textContent = deskripsiList[currentDeskripsiIndex];
     currentDeskripsiIndex = (currentDeskripsiIndex + 1) % deskripsiList.length;
 
-    // Update deskripsi setiap 5 detik
     setInterval(updateDeskripsi, 5000);
 });
 
