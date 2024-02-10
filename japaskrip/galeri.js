231e364dd97c02d423d60ef4f320253c88247c06
aui_tuthousend <mraihan286@gmail.com>
2024-02-10 13:40:10 +0700

    feat: profile add picture & desc

--- japaskrip/galeri.js
+++ japaskrip/galeri.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageContainer = document.getElementById("imageContainer");
-    const imageSources = ["images/788505.png", "images/1288255.jpg", "images/961255.png"]; // Add more image sources as needed
+    const imageSources = ["images/788505.png", "images/1288255.jpg", "images/961255.png", "https://images.unsplash.com/photo-1666091863721-54331a5db52d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80", "https://images.unsplash.com/photo-1666005487638-61f45819c975?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+        "https://images.unsplash.com/photo-1665910407771-bc84ad45676b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"]; // Add more image sources as needed
 
     let currentImageIndex = 0;
 
@@ -27,7 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     const imageContainer = document.getElementById("imageContainer2");
-    const imageSources = ["images/961255.png", "images/788505.png", "images/1288255.jpg"]; // Add more image sources as needed
+    const imageSources = ["images/961255.png", "images/788505.png", "images/1288255.jpg", "https://images.unsplash.com/photo-1666005487638-61f45819c975?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+        ,"https://images.unsplash.com/photo-1665910407771-bc84ad45676b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"]; // Add more image sources as needed
 
     let currentImageIndex = 0;
 
