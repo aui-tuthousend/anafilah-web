ed65d3c827da43390d1025b73608b644151b857d
aui_tuthousend <mraihan286@gmail.com>
2024-02-20 09:01:18 +0700

    feat: profile (transition left right done)

--- japaskrip/profile.js
+++ japaskrip/profile.js
@@ -10,6 +10,22 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 
-
 const hiddentxt = document.querySelectorAll('.hidden');
 hiddentxt.forEach((el) => observer.observe(el));
+
+//  <----------------------------         --------------------------------------------------------->
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show2');
+        } else {
+            entry.target.classList.remove('show2');
+        }
+    }));
+});
+
+
+const hiddentxt2 = document.querySelectorAll('.hidden2');
+hiddentxt2.forEach((el) => observer2.observe(el));
